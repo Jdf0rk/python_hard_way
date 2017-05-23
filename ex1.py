3972c5e42284e3194e6081eb0e21116c4a27d3ed
Unknown <mdesousa@etud.insa-toulouse.fr>
2017-05-23 11:48:14 -0400

    Ex1

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print "Hello world!"
+print "hello Again"
+print "I like typing this."
+print "this is fun"
+print "yay! Printing!"
+print "I'd much rather you 'not"
+print 'I "said" do not touch this'
+
+print "Test en français, avec des accents : éèëê äâaà cç "
