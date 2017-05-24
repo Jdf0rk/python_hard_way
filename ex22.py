aaa666a8860363ab6f1522eeb3a75e03ac9b623c
Unknown <mdesousa@etud.insa-toulouse.fr>
2017-05-24 11:16:41 -0400

    Ex21 - 22

---
+++ ex22.py
@@ -0,0 +1,57 @@
+#From ex1
+# print s a keyword to prompt anything on the screen
+
+#From ex2
+# # is the symbol for a comment (Obviously bro')
+
+#From ex3
+# la , sur le print permet de mettre un espace.
+
+#From ex4
+# typage dynamique
+
+#From ex5
+# %d pour un décimal %s pour string %r pour donnée brute
+
+#From ex6
+#True False :Vrai faux
+
+
+#From ex7
+# '+' sur un print concatène les chaines
+
+#From ex8
+# print formatter % (%d,%s,%s,%d)
+
+#From ex9
+# Utilisation de """ pour déclarer un bloc de string
+
+#From ex10
+# le \ backslash pour les metacaractère et l'échappement de caractères
+
+#From ex11
+# raw_input(motif)
+
+#From ex13
+# from sys import argv == depuis la librairie sys, on utilise argv
+# script, first, second, third = argv  argv restitue le nom du programme/script et les 3 arguments.
+
+#From ex15
+# FILEOBJECT f = open(filename)
+# f.read() f.write()
+# f.close()
+# f.truncate(size in ko)
+
+
+#From ex17
+# from os.path import exists  ==  on import de la librairie os.path la notion de exists
+
+
+#From ex18
+# def print_two(*args):    Déclaration d'une fonction
+# * nombre d'arguments non défini
+
+
+#From 20
+# f.seek(caractère a viser)
+# f.readline() lire la ligne complète
