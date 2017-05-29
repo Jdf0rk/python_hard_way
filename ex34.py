3c680640d49c9a23660553191a44d668687efe08
Unknown <mdesousa@etud.insa-toulouse.fr>
2017-05-29 05:06:43 -0400

    ex27-35

---
+++ ex34.py
@@ -0,0 +1,27 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+
+#The animal at 1.
+print "The animal at 1 is : %s" % animals[1]
+
+#The third (3rd) animal.
+print "The animal third is : %s" % animals[2]
+
+#The first (1st) animal.
+print "The animal at first is : %s" % animals[0]
+
+#The animal at 3.
+print "The animal at 3 is : %s" % animals[3]
+
+#The fifth (5th) animal.
+print "The animal at fith place is : %s" % animals[4]
+
+#The animal at 2.
+print "The animal at 2 is : %s" % animals[2]
+
+#The sixth (6th) animal.
+print "The animal at sixth place is : %s" % animals[5]
+
+#The animal at 4.
+print "The animal at 4 is : %s" % animals[4]
